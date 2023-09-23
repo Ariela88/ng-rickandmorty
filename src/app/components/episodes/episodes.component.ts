5f2fc467fc8942d932204f92d6cfcba24e2869ba
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-09-23 11:40:32 +0200

    bug resolt

--- src/app/components/episodes/episodes.component.ts
+++ src/app/components/episodes/episodes.component.ts
@@ -4,6 +4,7 @@ import { Episode } from 'src/app/model/episode';
 import { ConnectionService } from 'src/app/service/connection.service';
 import { SharedService } from 'src/app/service/shared.service';
 import { Router } from '@angular/router';
+import { Observable, forkJoin } from 'rxjs';
 import { Character } from 'src/app/model/character';
 
 @Component({
@@ -14,39 +15,70 @@ import { Character } from 'src/app/model/character';
 export class EpisodesComponent implements OnInit {
 
   episodeDetails: Episode | null = null;
-  episodeData: Episode[] = []
-  characters?:Character
+  episodeData: Episode[] = [];
+  characters?: Character[];
 
   constructor(
+    public connection: ConnectionService,
     private route: ActivatedRoute,
-    private sharedService: SharedService,
-    private router:Router
+    private router: Router,
+    private sharedService: SharedService
   ) {}
 
   ngOnInit(): void {
-    this.sharedService.selectedEpisode$.subscribe(
-      (episode: Episode | null) => {
-        if (episode) {
-          this.episodeDetails = episode;
-          console.log(episode);
-        } else {
-          this.episodeDetails = null; 
-          console.log("Nessun episodio trovato.");
-        }
-      },
-      (error) => {
-        console.log("Errore nel recupero dell'episodio:", error);
-      },
-      () => {
-        console.log("Completato"); 
-      }
+    // this.sharedService.selectedEpisode$.subscribe(
+    //   (episode: Episode | null) => {
+    //     if (episode) {
+    //       this.episodeDetails = episode;
+    //     } else {
+    //       this.episodeDetails = null; 
+    //       console.log("Nessun episodio trovato.");
+    //     }
+    //   },
+    //   (error) => {
+    //     console.log("Errore nel recupero dell'episodio:", error);
+    //   },
+    //   () => {
+    //     console.log("Completato"); 
+    //   }
+    // );
+    const episodeIndex = parseInt(
+      this.route.snapshot.paramMap.get('id') || '0',
+      10
     );
+
+    if (!isNaN(episodeIndex)) {
+      this.connection.getEpisodeById(episodeIndex).subscribe({
+        next: (epDetail) => {
+          this.episodeDetails = epDetail;
+
+          if (epDetail.characters) {
+            const characterID: number[] = epDetail.characters.map(
+              (characterUrl: string) => {
+                const parts = characterUrl.split('/');
+                return parseInt(parts[parts.length - 1], 10);
+              }
+            )
+
+
+            const characterObservable: Observable<Character>[] = characterID.map(
+              (characterID) => this.connection.getCharWithIndex(characterID)
+            );
+
+            forkJoin(characterObservable).subscribe({
+              next: (characters) => {
+                this.characters = characters;
+              },
+              error: (error) => console.log(error),
+            });
+          }
+
+          // console.log(charDetail);
+        },
+        error: (error) => console.log(error),
+      });
+    }
   }
-  
-  
-  // select(episode: Episode) {
-  
-  //   this.router.navigateByUrl('/details/' + episode.characters[0].id);
-  // }
+
 
 }
